2bd5c22ac0aec4f4ea53cd28a862b13a9aa68b73
srirambalaji17 <31446363+srirambalaji17@users.noreply.github.com>
2018-03-10 14:20:48 +0530

    Add files via upload

---
+++ beginner/positive negative or zero.py
@@ -0,0 +1,19 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      rgcet
+#
+# Created:     10/03/2018
+# Copyright:   (c) rgcet 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+     import math
+
+number = int (input ('Enter number: '))
+if number % 2 == 0 and number != 0:
+        print ('Even number')
+else:
+         print ('Odd number')
